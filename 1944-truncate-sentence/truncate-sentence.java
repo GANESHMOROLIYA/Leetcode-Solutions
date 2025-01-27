dd360f6a63cccce3bf2b9a6cf4e3553209166096
Ganesh Narayan Moroliya <158822512+GANESHMOROLIYA@users.noreply.github.com>
2025-01-27 17:03:29 +0530

    Time: 2 ms (52.50%) | Memory: 42.3 MB (40.82%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] str=s.split(" ");
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<k-1;i++){
+            sb.append(str[i] +" ");
+        }
+        sb.append(str[k-1]); 
+        return sb.toString();
+    }
+}
