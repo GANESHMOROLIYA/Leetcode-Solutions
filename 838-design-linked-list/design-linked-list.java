f9782d2c4f1ce17d41f7a08301b0d1dfe2edd0fa
Ganesh Narayan Moroliya <158822512+GANESHMOROLIYA@users.noreply.github.com>
2025-06-07 17:33:57 +0530

    Time: 7 ms (84.32%) | Memory: 45.6 MB (68.46%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.java
@@ -0,0 +1,86 @@
+class MyLinkedList {
+    private static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) return -1;
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp.data;
+    }
+
+    public void addAtHead(int val) {
+        Node temp = new Node(val);
+        if (head == null) {
+            head = tail = temp;
+        } else {
+            temp.next = head;
+            head = temp;
+        }
+        size++;
+    }
+
+    public void addAtTail(int val) {
+        Node temp = new Node(val);
+        if (head == null) {
+            head = tail = temp;
+        } else {
+            tail.next = temp;
+            tail = temp;
+        }
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) return;
+        if (index == 0) {
+            addAtHead(val);
+        } else if (index == size) {
+            addAtTail(val);
+        } else {
+            Node temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            Node newNode = new Node(val);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            size++;
+        }
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+        if (index == 0) {
+            head = head.next;
+            if (head == null) tail = null;  // if list becomes empty
+        } else {
+            Node temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (index == size - 1) {
+                tail = temp;  // update tail if last node is removed
+            }
+        }
+        size--;
+    }
+}
