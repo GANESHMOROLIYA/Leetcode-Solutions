007248ede04e74280d04a4fd23e1a5edd8e09ff7
Ganesh Narayan Moroliya <158822512+GANESHMOROLIYA@users.noreply.github.com>
2025-06-08 14:27:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (28.17%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -20,14 +20,34 @@ public class Solution {
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null){
-            if(slow==null) return false;
+            if(slow==null){
+                return false;
+            }
             slow=slow.next;
             if(fast.next==null) return false;
             fast=fast.next.next;
-            if(fast==slow){
+            if(slow==fast){
                 return true;
             }
         }
         return false;
+        // if(head==null){
+        //     return false;
+        // }
+        // if(head.next==null){
+        //     return false;
+        // }
+        // ListNode slow=head;
+        // ListNode fast=head;
+        // while(fast!=null){
+        //     if(slow==null) return false;
+        //     slow=slow.next;
+        //     if(fast.next==null) return false;
+        //     fast=fast.next.next;
+        //     if(fast==slow){
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
 }
