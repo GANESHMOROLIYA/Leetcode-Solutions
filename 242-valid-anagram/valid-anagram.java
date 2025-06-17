874eeba3546d2af5883ccd8bd0d133a294a4f1e3
Ganesh Narayan Moroliya <158822512+GANESHMOROLIYA@users.noreply.github.com>
2025-06-17 17:41:17 +0530

    Time: 21 ms (10.91%) | Memory: 44.9 MB (25.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,26 +1,53 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character,Integer> mp1=new HashMap<>();
-        Map<Character,Integer> mp2=new HashMap<>();
+        //First approach 
+    //     Map<Character,Integer> mp1=new HashMap<>();
+    //     Map<Character,Integer> mp2=new HashMap<>();
         
+    //     if(s.length()!=t.length()){
+    //         return false;
+    //     }
+    //     for(int i=0;i<s.length();i++){
+    //         if(!mp1.containsKey(s.charAt(i))){
+    //             mp1.put(s.charAt(i),1);
+    //         }else{
+    //             mp1.put(s.charAt(i),mp1.get(s.charAt(i))+1);
+    //         }
+    //     }
+    //     for(int i=0;i<t.length();i++){
+    //         if(!mp2.containsKey(t.charAt(i))){
+    //             mp2.put(t.charAt(i),1);
+    //         }else{
+    //             mp2.put(t.charAt(i),mp2.get(t.charAt(i))+1);
+    //         }
+    //     }
+
+    //   return mp1.equals(mp2);
+    //Secound apporoach
         if(s.length()!=t.length()){
             return false;
         }
-        for(int i=0;i<s.length();i++){
-            if(!mp1.containsKey(s.charAt(i))){
-                mp1.put(s.charAt(i),1);
+         Map<Character,Integer> mp=new HashMap<>();
+         for(int i=0;i<s.length();i++){
+            if(!mp.containsKey(s.charAt(i))){
+                mp.put(s.charAt(i),1);
             }else{
-                mp1.put(s.charAt(i),mp1.get(s.charAt(i))+1);
+                mp.put(s.charAt(i),mp.get(s.charAt(i))+1);
             }
         }
         for(int i=0;i<t.length();i++){
-            if(!mp2.containsKey(t.charAt(i))){
-                mp2.put(t.charAt(i),1);
+            char ch=t.charAt(i);
+            if(!mp.containsKey(ch)){
+                return false;
             }else{
-                mp2.put(t.charAt(i),mp2.get(t.charAt(i))+1);
+               int val=mp.get(ch);
+               mp.put(ch,val-1);
             }
+            
         }
-
-      return mp1.equals(mp2);
+        for(int i:mp.values()){
+            if(i!=0) return false;
+        }
+        return true;
     }
 }
