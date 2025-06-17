9fbf099f1c9bb2491ffc6a2e38ddac8d9f7898bd
Ganesh Narayan Moroliya <158822512+GANESHMOROLIYA@users.noreply.github.com>
2025-06-17 18:20:38 +0530

    Time: 11 ms (67.52%) | Memory: 42.7 MB (37.32%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,6 +1,27 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        Map<Character,Character> mp=new HashMap<>();
+        // Map<Character,Character> mp=new HashMap<>();
+        // if(s.length()!=t.length()){
+        //     return false;
+        // }
+
+        // for(int i=0;i<s.length();i++){
+        //     char ch1=s.charAt(i);
+        //     char ch2=t.charAt(i);
+        //     if(mp.containsKey(ch1)){
+        //          if(mp.get(ch1)!=ch2){
+        //             return false;
+        //         }
+        //     }else if(mp.containsValue(ch2)){
+        //         return false;
+        //     }else{
+        //         mp.put(ch1,ch2);
+        //     }
+        // }
+        // return true;
+
+         Map<Character,Character> mp=new HashMap<>();
+         Set<Character> se=new HashSet<>();
         if(s.length()!=t.length()){
             return false;
         }
@@ -12,13 +33,15 @@ class Solution {
                  if(mp.get(ch1)!=ch2){
                     return false;
                 }
-            }else if(mp.containsValue(ch2)){
+            }else if(se.contains(ch2)){
                 return false;
             }else{
                 mp.put(ch1,ch2);
+                se.add(ch2);
             }
         }
         return true;
+
 
         
     }
