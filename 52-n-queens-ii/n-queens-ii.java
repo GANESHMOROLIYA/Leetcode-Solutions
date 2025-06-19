6747d61994cd5423e6d26ce85e2f2823534fa290
Ganesh Narayan Moroliya <158822512+GANESHMOROLIYA@users.noreply.github.com>
2025-06-19 15:50:42 +0530

    Time: 12 ms (5.36%) | Memory: 42.9 MB (5.58%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -6,21 +6,21 @@ class Solution {
                 board[i][j]='.';
             }
         }
-        List<List<String>> ans=new ArrayList<>();
-        Nqueen(board,0,ans);
-        return ans.size();
+         List<List<String>> ans=new ArrayList<>();
+         Nqueen(board,0,ans);
+         return ans.size();
+
     }
      public void Nqueen(char[][] board,int row, List<List<String>> ans) {
         int n=board.length;
         if(row==n){
-            //copy 2D char array into list of String 
-            List<String> l=new ArrayList<>();
+             List<String> l=new ArrayList<>();
             for(int i=0;i<n;i++){
                 String str="";
                 for(int j=0;j<n;j++){
-                    str+=board[i][j];
+                   str+=board[i][j];
                 }
-                l.add(str);
+                 l.add(str);
             }
             ans.add(l);
             return;
@@ -34,45 +34,41 @@ class Solution {
         }
     }
     public boolean isSafe(char[][] board,int row,int col){
-        int n= board.length;
+       int n=board.length;
+       for(int i=0;i<n;i++){
+        if(board[row][i]=='Q') return false;
+       }
         for(int j=0;j<n;j++){
-            if(board[row][j]=='Q') return false;
-        }
-        for(int i=0;i<n;i++){
-            if(board[i][col]=='Q') return false;
-        }
-        int i=row;
-        int j=col;
-        //nort east
-        while(i>=0 && j<n){
-            if(board[i][j]=='Q') return false;
-            i--;
-            j++;
-        }
-        //south east
-        i=row;
-        j=col;
-        while(i<n && j<n){
-            if(board[i][j]=='Q') return false;
-            i++;
-            j++;
-        }
-        //north west
-        i=row;
-        j=col;
-        while(i>=0 && j>=0){
-            if(board[i][j]=='Q') return false;
-            i--;
-            j--;
-        }
-        //west south
-        i=row;
-        j=col;
-        while(i<n && j>=0){
-            if(board[i][j]=='Q') return false;
-            i++;
-            j--;
-        }
-        return true;
+        if(board[j][col]=='Q') return false;
+       }
+       int i=row;
+       int j=col;
+       while(i>=0&&j<n){
+         if(board[i][j]=='Q') return false;
+          i--;
+          j++;
+       }
+       i=row;
+       j=col;
+        while(i<n&&j<n){
+         if(board[i][j]=='Q') return false;
+          i++;
+          j++;
+       }
+       i=row;
+       j=col;
+        while(i>=0&&j>=0){
+         if(board[i][j]=='Q') return false;
+          i--;
+          j--;
+       }
+       i=row;
+       j=col;
+        while(i<n&&j>=0){
+         if(board[i][j]=='Q') return false;
+          i++;
+          j--;
+       }
+       return true;
     }
 }
